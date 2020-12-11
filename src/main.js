7c5a9cbdae5c469fb2dc2dee1016d975aeade758
Steve_Zhang <wentaozhang0305@gmail.com>
2020-12-10 22:36:38 -0800

    installed package

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import "primevue/resources/themes/saga-blue/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
+import App from "./App.vue";
+import store from "./store";
+import router from "./router";
 
-createApp(App).mount('#app')
+createApp(App).use(router).use(store).mount("#app");
