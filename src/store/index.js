c98c7b51d2207271780777fff1a82a89916bddd1
Steve_Zhang <wentaozhang0305@gmail.com>
2020-12-12 22:58:37 -0800

    card submit

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,18 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
+    cards: [],
   },
   mutations: {
+    appendCard(state, card) {
+      state.cards.push(card);
+    },
   },
   actions: {
+    createCard({ commit }, card) {
+      commit("appendCard", card);
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,63 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home p-grid">
+    <Card class="p-col-6 p-offset-3">
+      <template v-slot:header>
+        <h2>Create Card</h2>
+      </template>
+
+      <template v-slot:content>
+        <form @submit.prevent="submitNewCard" class="p-fluid">
+          <div class="p-field">
+            <label for="question">Question</label>
+            <inputText v-model="question" class="p-field" id="question" />
+          </div>
+
+          <div class="p-field">
+            <label for="answer">Answer</label>
+            <inputText v-model="answer" class="p-field" id="answer" />
+          </div>
+
+          <Button type="submit" label="Add New Card"></Button>
+        </form>
+      </template>
+    </Card>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { mapActions, mapState } from "vuex";
+import InputText from "primevue/inputtext";
+import Button from "primevue/button";
+import Card from "primevue/card";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
+    InputText,
+    Button,
+    Card
+  },
+  data() {
+    return {
+      question: "",
+      answer: ""
+    };
+  },
+  computed: {
+    ...mapState(["card"])
+  },
+  methods: {
+    ...mapActions(["createCard"]),
+    submitNewCard() {
+      const payload = {
+        question: this.question,
+        answer: this.answer
+      };
+      this.createCard(payload);
+      this.question = "";
+      this.answer = "";
+    }
   }
-}
+};
 </script>
